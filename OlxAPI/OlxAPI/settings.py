a4b425322a03e74b2ceb5f4745acccb8844a97b2
Vaseem Cholakkan <vaseemcholakkan@gmail.com>
2023-06-26 20:56:29 +0530

    env file

--- OlxAPI/OlxAPI/settings.py
+++ OlxAPI/OlxAPI/settings.py
@@ -13,6 +13,9 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from pathlib import Path
 from datetime import timedelta
 import os
+import environ
+env = environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,8 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-uxn2q!!_@b43*_1wx4$w=fb$xk6xd5qfq)ds5rbdv-e#7ilo26"
+SECRET_KEY = env('SECRET_KEY')
 
+"django-insecure-uxn2q!!_@b43*_1wx4$w=fb$xk6xd5qfq)ds5rbdv-e#7ilo26"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
@@ -95,11 +99,11 @@ ASGI_APPLICATION = "OlxAPI.asgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": "olx",
+        "NAME": env('DATABASE_NAME'),
         # "USER": "vaseem",
-        "USER":"postgres",
+        "USER":env('DATABASE_USER'),
         "HOST": "localhost",
-        "PASSWORD": "1234",
+        "PASSWORD": env('DATABASE_PASS'),
     }
 }
 
