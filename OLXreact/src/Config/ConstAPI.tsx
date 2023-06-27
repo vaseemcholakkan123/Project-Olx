63679fcd1d6ab71b38b5cc0b25f52373d82ebb05
Vaseem Cholakkan <vaseemcholakkan@gmail.com>
2023-06-27 17:03:20 +0530

    cors added

--- OLXreact/src/Config/ConstAPI.tsx
+++ OLXreact/src/Config/ConstAPI.tsx
@@ -2,4 +2,4 @@
 // export const BASE_URL = 'http://127.0.0.1:8000/olx-api'
 // export const BASE_IMAGE_URL = 'http://127.0.0.1:8000'
 export const BASE_URL = 'https://moddroid.tk/olx-api'
-export const BASE_IMAGE_URL = 'https://moddroid.tk/'
+export const BASE_IMAGE_URL = 'https://moddroid.tk/olx-api/'
