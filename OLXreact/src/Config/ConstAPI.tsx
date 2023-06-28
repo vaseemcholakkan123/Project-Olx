3cd707ea298832355bddb6a2c4e083ee6c3dbaab
Vaseem Cholakkan <vaseemcholakkan@gmail.com>
2023-06-28 20:47:23 +0530

    time zone updated

--- OLXreact/src/Config/ConstAPI.tsx
+++ OLXreact/src/Config/ConstAPI.tsx
@@ -1,5 +1,5 @@
 
-export const BASE_URL = 'http://127.0.0.1:8000/olx-api'
-export const BASE_IMAGE_URL = 'http://127.0.0.1:8000'
-// export const BASE_URL = 'https://moddroid.tk/olx-api'
-// export const BASE_IMAGE_URL = 'https://moddroid.tk'
+// export const BASE_URL = 'http://127.0.0.1:8000/olx-api'
+// export const BASE_IMAGE_URL = 'http://127.0.0.1:8000'
+export const BASE_URL = 'https://moddroid.tk/olx-api'
+export const BASE_IMAGE_URL = 'https://moddroid.tk'
